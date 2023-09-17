6f6c61f537de28cf1e9ae0ad131b3477a593e348
Paul Scarrone <paul@scarrone.co>
2023-09-17 11:51:00 -0400

    added ruby and multi-shell tracking for kafka server

--- flake.nix
+++ flake.nix
@@ -7,19 +7,58 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
       kafka = pkgs.apacheKafka;
+      ruby = pkgs.ruby;
     in {
       defaultPackage.x86_64-linux = kafka;
       devShells.x86_64-linux.default = pkgs.mkShell {
         name = "My-project build environment";
         buildInputs = [
           kafka
+          ruby
         ];
         shellHook = ''
+          touch nix.env.local
+          handle_shell() {
+            local reason="$1"
+            if [ "$reason" = "register" ]; then
+              awk -F= '/^fork_count=/ {$2 = $2 + 1} {print $1 "=" $2}' nix.env.local > tmp_nix.env.local
+            elif [ "$reason" = "unregister" ]; then
+              awk -F= '/^fork_count=/ {$2 = $2 - 1} {print $1 "=" $2}' nix.env.local > tmp_nix.env.local
+            fi
+            cat tmp_nix.env.local
+            if [ -f tmp_nix.env.local ]; then
+              mv tmp_nix.env.local nix.env.local
+            fi
+          }
+
+          no_more_shells() {
+            fork_count=$(grep '^fork_count=' nix.env.local | cut -d= -f2)
+            if [ "$fork_count" -eq 0 ]; then
+              is_zero=0
+            else
+              is_zero=1
+            fi
+
+            echo $is_zero
+          }
+
+          deprovision() {
+            handle_shell "unregister"
+            shell_count=$(no_more_shells)
+            if [ "$shell_count" -eq 0 ]; then
+              echo "Last Shell Closing Shutting Kafka Down"
+              kafka-server-stop.sh
+            fi
+          }
+
+          handle_shell "register"
           echo "Welcome in $name"
           KAFKA_CLUSTER_ID="$(kafka-storage.sh random-uuid)"
           kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c kraft.properties
           kafka-server-start.sh -daemon kraft.properties
-          trap "kafka-server-stop.sh" EXIT
+
+          trap deprovision EXIT
+          bundle install
         '';
       };
     };
